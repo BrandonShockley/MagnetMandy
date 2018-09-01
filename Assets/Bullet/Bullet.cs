360888a5bb81191f8c43de3d760b5a154e8e13ea
Brandon Shockley <bshoc@bellsouth.net>
2018-09-01 12:30:42 -0400

    Added bullet influencing

--- Assets/Bullet/Bullet.cs
+++ Assets/Bullet/Bullet.cs
@@ -11,14 +11,22 @@ public class Bullet : MonoBehaviour {
     }
 
     //When created from BulletShooter
-    public void Shoot(Vector2 direction, float speed)
-	{
+    public void Shoot(Vector2 direction, float speed) {
         rb.velocity = direction.normalized * speed;
+        UpdateHeading();
 	}
 
 	// Move Bullet
 	void Update () {
+
 	}
 
-	
+	public void InfluenceTrajectory(Vector2 direction) {
+        rb.AddForce(direction, ForceMode2D.Force);
+        UpdateHeading();
+    }
+
+    private void UpdateHeading() {
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(rb.velocity.y, rb.velocity.x) - 90f);
+    }
 }
---
+++ Assets/Player/MagnetCone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetCone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Bullet")) {
+            other.GetComponent<Bullet>().InfluenceTrajectory(transform.parent.position - other.transform.position);
+        }
+    }
+}
