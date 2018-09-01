d71aa6d708eb9742ff87ff4361f7321119a4e95b
Brandon Shockley <bshoc@bellsouth.net>
2018-08-31 21:07:59 -0400

    Setup player folder

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 playerToMouse = mousePosition - (Vector2)transform.position;
+
+        transform.rotation = Quaternion.LookRotation(playerToMouse);
+	}
+}
