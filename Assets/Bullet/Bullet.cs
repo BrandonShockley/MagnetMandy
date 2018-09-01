904625fb8397a6de86a1a88c7343c8f81f9a0fc8
Andrew Hoyt <andrew@chillhammer.com>
2018-08-31 22:39:14 -0400

    Bullet, Bullet Shooter, Bullet Dispenser Debug
    To get that aspect up and running

---
+++ Assets/Bullet/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	[SerializeField] Vector2 direction;
+	[SerializeField] float speed;
+
+	//When created from BulletShooter
+	public void Shoot(Vector2 direction, float speed)
+	{
+		this.direction = direction.normalized;
+		this.speed = speed;
+	}
+
+	// Move Bullet
+	void Update () {
+		transform.position = transform.position + (Vector3)(direction * speed * Time.deltaTime);
+	}
+
+	
+}
---
+++ Assets/Bullet/BulletShooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour {
+
+	public GameObject bullet;
+	[Range(0, 5)]
+	public float defaultSpeed = 1f;
+
+	[Range(0, 90)]
+	public float angleNoise = 0f; //noise in angle offset
+
+	public void ShootBullet(Vector2 direction, Vector2 startPosition, float speed)
+	{
+		GameObject bulletInst = Instantiate(bullet, startPosition, Quaternion.FromToRotation(Vector3.up, direction), transform);
+		bulletInst.GetComponent<Bullet>().Shoot(direction, speed);
+	}
+
+	public void ShootBullet(Vector2 direction, Vector2 startPosition)
+	{
+		ShootBullet(direction, startPosition, defaultSpeed);
+	}
+
+	public void ShootBullet(Vector2 direction)
+	{
+		ShootBullet(direction, transform.position);
+	}
+}
