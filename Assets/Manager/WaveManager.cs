ce242ad80a1f7c277d2006146bbfb2341c6065ed
Brandon Shockley <bshoc@bellsouth.net>
2018-09-01 17:31:11 -0400

    Working on title screen

--- Assets/Manager/WaveManager.cs
+++ Assets/Manager/WaveManager.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour {
 
+    private List<List<Transform>> waves;
+
+    private int currentWave = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+        //Init list of waves
+		for (int i = 0; i < transform.childCount; i++) {
+            waves.Add(new List<Transform>());
+            for (int j = 0; j < transform.GetChild(i).childCount; j++) {
+                waves[i].Add(transform.GetChild(i).GetChild(j));
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void SpawnWave() {
+        foreach (Transform enemy in waves[currentWave]) {
+            enemy.gameObject.SetActive(true);
+        }
+    }
 }
