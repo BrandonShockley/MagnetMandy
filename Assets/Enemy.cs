5645279069afd89a4e8050e151feabff95efb980
Andrew Hoyt <andrew@chillhammer.com>
2018-09-01 13:42:17 -0400

    Enemy Locomotion Script Setup
    Moving setup

---
+++ Assets/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyLocomotion))]
+public class Enemy : MonoBehaviour {
+
+	EnemyLocomotion movement;
+
+	void Start () {
+		movement = GetComponent<EnemyLocomotion>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void MoveTowardsPlayer()
+	{
+
+	}
+}
---
+++ Assets/EnemyLocomotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLocomotion : MonoBehaviour {
+
+	[Range(0,1)]
+	public float accelerationSpeed = 0.2f;
+
+	[Range(0, 1)]
+	public float decelerationSpeed = 0.2f;
+
+	[Range(0, 5)]
+	public float maxSpeed;
+
+	Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
